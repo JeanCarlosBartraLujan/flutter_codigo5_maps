be1555a64889bd71c769f63e5c0ce378e452110e
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-08 20:18:40 -0500

    Init 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,17 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: LatLng(0,0)),
+      ),
+    );
   }
 }
