34e22f26c7daf8665f1369b844d5babf1691b527
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-08 23:05:50 -0500

    Init 5

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
+
+  Future<CameraPosition> initCurrentPosition() async {
+    Position position = await Geolocator.getCurrentPosition();
+    return  CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 15,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    initCurrentPosition();
+
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(-16.384416, -71.540073),
-          zoom: 15,
-        ),
+      body: FutureBuilder(
+        future: initCurrentPosition(),
+        builder: (BuildContext context, AsyncSnapshot snap){
+          if(snap.hasData){
+            return GoogleMap(
+              initialCameraPosition: snap.data,
+              myLocationEnabled: true,
+            );
+          }
+          return Center(child: CircularProgressIndicator(),);
+        },
       ),
     );
   }
