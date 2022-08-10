8c63b73a0a02f9d84ac89104f1027d1d3d3bf990
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-08-09 19:29:21 -0500

    Google Maps 1.2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,12 +5,17 @@ import 'package:flutter_codigo5_maps/utils/map_style.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
-  GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Set<Marker> _markers = {};
 
   Future<CameraPosition> initCurrentPosition() async {
     Position position = await Geolocator.getCurrentPosition();
-    return  CameraPosition(
+    return CameraPosition(
       target: LatLng(position.latitude, position.longitude),
       zoom: 15,
     );
@@ -23,18 +28,36 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: FutureBuilder(
         future: initCurrentPosition(),
-        builder: (BuildContext context, AsyncSnapshot snap){
-          if(snap.hasData){
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (snap.hasData) {
             return GoogleMap(
               initialCameraPosition: snap.data,
               myLocationEnabled: true,
+              myLocationButtonEnabled: false,
               mapType: MapType.normal,
-              onMapCreated: (GoogleMapController googleController){
+              compassEnabled: true,
+              onMapCreated: (GoogleMapController googleController) {
                 googleController.setMapStyle(jsonEncode(mapStyle));
+              },
+              markers: _markers,
+              onTap: (LatLng position) {
+
+                Marker marker = Marker(
+                  markerId: MarkerId("00001"),
+                  position: position
+                );
+
+                _markers.add(marker);
+
+                setState((){});
+
+
               },
             );
           }
-          return const Center(child: CircularProgressIndicator(),);
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
